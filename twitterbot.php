dc581e2d6a5d78d6e860f1dda7fbf9ac78953e79
David Triebwasser <david@skyou.com>
2016-04-22 23:20:30 -0700

    Added files via upload
    updated with separate credential file

---
+++ twitterbot.php
@@ -0,0 +1,57 @@
+<?php
+// twitteroauth setup
+require "vendor/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+require_once('secretFile');
+
+// simplepie setup
+require_once('php/autoloader.php');
+
+// array of bot comments
+$comment = array("Was not expecting that","Not sure what to make of it","Found this today", "From the news vault", "another thing happened", "just saw this", "in the news recently");
+
+// create SimplePie object and get random feed item
+$feed = new SimplePie();
+$feed->set_feed_url('http://news.google.com/?output=atom');
+$feed->init();
+$feed->handle_content_type();
+$number = rand(1,7);
+$item = $feed->get_item($number);
+
+// pull down title and review first. if blocker found, don't add comment
+
+$newsTitle = $item->get_title();
+$block = array("dead","killed","murder","rape");
+$flag = 0;
+
+foreach($block as $blocker){
+ if ( strpos($newsTitle,$blocker) ) {
+   $flag = 1;
+   //no comment
+ }
+}
+
+// Add link to source content, not Google link
+$link = $item->get_link();
+$linkArray = explode('url=',$link);
+$offLink = array_pop($linkArray);
+
+if ($flag == 0) { $newsTitle = $comment[--$number] . "... " . $newsTitle; }
+$newsTitle = $newsTitle . " " . $offLink;
+
+// Separate credentials from code
+$connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+
+
+// test connection to account
+//$content = $connection->get("account/verify_credentials");
+
+// post news to Twitter account
+$statuses = $connection->post("statuses/update", ["status" => $newsTitle]);
+
+// check output of response in human readable format
+//print_r($statuses);
+//print_r($content);
+
+?>
